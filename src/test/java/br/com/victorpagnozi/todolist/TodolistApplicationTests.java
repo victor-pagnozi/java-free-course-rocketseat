4e21bcdc18d32edb9bbbdc3fb09db07e85b573ac
victor-pagnozi <victor.pagnozi@gmail.com>
2023-10-15 23:15:36 -0300

    feat(lesson 1,2,3):: Created java project && done user structure && done task structure && started filters of authorization requests

---
+++ src/test/java/br/com/victorpagnozi/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorpagnozi.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
